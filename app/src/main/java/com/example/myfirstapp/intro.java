c1eeebe33260cc5f6a3c0090e9a7bfdf4fd22441
Jack McDonald <jackm3@illinois.edu>
2019-04-25 14:01:08 -0500

    This includes working base alarm to modify now! (I think it is working or close to working). Be careful to not erase your code when you merge or pull this.

--- app/src/main/java/com/example/myfirstapp/intro.java
+++ app/src/main/java/com/example/myfirstapp/intro.java
@@ -1,11 +1,56 @@
 package com.example.myfirstapp;
 
+import android.media.Ringtone;
+import android.media.RingtoneManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextClock;
+import android.widget.TimePicker;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class intro extends AppCompatActivity {
+
+    TextClock currentTime;
+    TimePicker alarmTime;
+
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro);
+
+        currentTime = findViewById(R.id.textClock);
+        alarmTime = findViewById(R.id.timePicker);
+        final Ringtone simpleRingtone = RingtoneManager.getRingtone(getApplicationContext(),
+            RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
+        Timer checkTime = new Timer();
+        checkTime.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if (currentTime.getText().toString().equals(AlarmTime())) {
+                    simpleRingtone.play();
+                } else {
+                    simpleRingtone.stop();
+                }
+            }
+        }, 0, 2000);
+    }
+    public String AlarmTime() {
+        int hour = alarmTime.getCurrentHour();
+        int minute = alarmTime.getCurrentMinute();
+        String minuteString;
+        if (minute < 10) {
+            minuteString = "0" + minute;
+        } else {
+            minuteString = minute + "";
+        }
+        String alarmTimeAsString;
+        if (hour > 12) {
+            hour = hour - 12;
+            alarmTimeAsString = (hour + " : " + minuteString + " PM");
+        } else {
+            alarmTimeAsString = (hour + " : " + minuteString + " PM");
+        }
+        return alarmTimeAsString;
     }
 }
